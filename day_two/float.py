625cb7083452ce3d885c0ac3541ad5bcb5dc631f
JiangWang <alex1989wang@foxmail.com>
2017-07-06 18:29:10 +0800

    day two practice; python is really powerful with string manipulations.

---
+++ day_two/float.py
@@ -0,0 +1,26 @@
+
+print(float('334e1'))
+print(float('20.3'))
+
+string = 'Release the kraken!' + 'At once.'
+print(string)
+
+a = "hello"
+b = a
+c = "world"
+print(a + b + c)
+print(a, b, c)
+
+start = 'Na ' * 4 + '\n'
+middle ='Hey ' * 3 + '\n'
+end = 'Goodbye'
+print(start + start + middle + end)
+
+a = '0123456789'
+print(a[0], a[-1])
+#print(a[-11])
+
+#string slicing 
+name = 'Penny'
+new_name = 'H' + name[1:]
+print(new_name)
---
+++ day_two/string.py
@@ -0,0 +1,11 @@
+
+#string slicing 
+name = 'Penny'
+new_name = 'H' + name[1:]
+print(new_name)
+
+new_new_name = name[:3] + 'hehe' + name[-1:]
+print(new_new_name)
+
+name_backward = name[-1::-1]
+print(name_backward)
