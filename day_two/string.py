3381f4a64a81f8dcfdce200c605d6771646e9224
JiangWang <alex1989wang@foxmail.com>
2017-07-06 20:08:33 +0800

    add python gitignore file.

--- day_two/string.py
+++ day_two/string.py
@@ -9,3 +9,27 @@ print(new_new_name)
 
 name_backward = name[-1::-1]
 print(name_backward)
+
+print(len(name_backward))
+print(len(''))
+
+# string split example
+string_to_split = 'like hello, world, i think i like you'
+split_strings = string_to_split.split(',')
+print(split_strings)
+
+# string join 
+join_string = ','.join(split_strings)
+print(join_string)
+
+# string manipulation 
+print(string_to_split.rfind('like'))
+print(string_to_split.count('like'))
+print(string_to_split.isalnum())
+print(string_to_split.strip('like'))
+print(string_to_split.title())
+print(string_to_split.center(30)) #will be effective in interactive mode
+print(string_to_split.rjust(30)) #will be effective in interactive mode
+
+# watch out for substution 
+print(string_to_split.replace('o', 'oh my God'))
